fa401421d7544e9c0c1748cce1e3324d8a312964
Mindy Preston <meetup@yomimono.org>
2015-03-30 15:20:16 +0100

    test_irmin_arp: merge conflict test added, but its behavior is definitely incorrect

--- lib_test/test_irmin_arp.ml
+++ lib_test/test_irmin_arp.ml
@@ -10,14 +10,14 @@ let parse ip mac time = (Ipaddr.V4.of_string_exn ip, Macaddr.of_string_exn
                            mac, time)
 let confirm time mac = Entry.make_confirmed time mac
 
+let ip1, mac1, time1 = parse "192.168.3.11" "10:9a:dd:00:00:11" 0.0
+let ip2, mac2, time2 = parse "192.168.3.22" "00:16:3e:00:00:22" 1.5
+let ip3, mac3, time3 = parse "192.168.3.50" "a0:23:4a:00:00:50" 2.0
+
 let make_in_memory () =
   let store = Irmin.basic (module Irmin_mem.Make) (module T) in
   let config = Irmin_mem.config () in
   Irmin.create store config Irmin_unix.task
-
-let ip1, mac1, time1 = parse "192.168.3.11" "10:9a:dd:00:00:11" 0.0
-let ip2, mac2, time2 = parse "192.168.3.22" "00:16:3e:00:00:22" 1.5
-let ip3 = Ipaddr.V4.of_string_exn "192.168.3.50"
 
 let assert_in m k =
   OUnit.assert_equal ~printer:string_of_bool (Ipv4_map.mem k m) true
@@ -60,14 +60,15 @@ let readback_works _ctx =
   assert_pending m ip3;
   return_unit
 
-let updates_work _cts =
+let simple_update_works _cts =
   let node = T.Path.empty in
   let original = T.of_map (old ()) in
   make_in_memory () >>= fun t ->
   Irmin.update (t "original map") node original >>= fun () ->
   (* clone, branch, update, merge *)
   (* all the examples use `clone_force`, which just overwrites on name
-     collisions *)
+     collisions, but a name collision would be bizarre enough that we should
+     probably just invalidate the test *)
   Irmin.clone Irmin_unix.task (t "clone original map") "age_out" >>= function
   | `Duplicated_tag ->
     OUnit.assert_failure "tag claimed to be duplicated on a fresh in-memory
@@ -95,9 +96,72 @@ let updates_work _cts =
     OUnit.assert_equal new_m_from_t m;
     return_unit
 
+(* make sure our facility for automatically solving merge conflicts is working
+   as expected *)
+let merge_conflicts_solved _ctx = 
+  let resolve_pending branch node =
+    Irmin.read_exn (branch "read map") node >>= fun map ->
+    let map = T.to_map map in
+    Lwt.return (Ipv4_map.add ip3 (confirm time3 mac3) map)
+  in
+  let remove_expired branch node =
+    Irmin.read_exn (branch "read map") node >>= fun map ->
+    let map = T.to_map map in
+    Lwt.return (Ipv4_map.remove ip1 map)
+  in
+  make_in_memory () >>= fun t ->
+  (* initialize data *) 
+  let node = T.Path.empty in
+  let original = T.of_map (old ()) in
+  Irmin.update (t "original map") node original >>= fun () ->
+  (* make two branches *)
+  let pending_resolved = 
+    Irmin.clone Irmin_unix.task (t "clone map")
+      "pending_resolved" >>= function
+    | `Duplicated_tag -> 
+      OUnit.assert_failure "tag claimed to be duplicated on a fresh in-memory
+    store"
+    | `Ok resolved_branch ->
+      (* set previously-pending entry ip3 to a value (arp reply received!) *)
+      resolve_pending resolved_branch node >>= fun map -> 
+      (* write it back *)
+      Irmin.update (resolved_branch "resolve arp entry") node (T.of_map map) >>=
+      fun () -> Lwt.return resolved_branch
+  in
+  let expired_removed = 
+    Irmin.clone Irmin_unix.task (t "clone map") "expired_removed" >>= function
+    | `Duplicated_tag ->
+      OUnit.assert_failure "tag claimed to be duplicated on an almost-fresh in-memory
+    store"
+    | `Ok remove_branch ->
+      remove_expired remove_branch node >>= fun map ->
+      Irmin.update (remove_branch "remove expired entries") node (T.of_map map)
+        >>= fun () -> Lwt.return remove_branch
+  in
+  pending_resolved >>= fun pend_branch -> 
+  expired_removed >>= fun exp_branch ->
+  (* both branches (expired_removed, pending_resolved) should now be written
+     into Irmin store *)
+  (* try merging first one, then the other, into master (t) *)
+  Irmin.merge "Merging pending_resolved into into master" pend_branch
+    ~into:t >>= function
+  | `Conflict s -> OUnit.assert_failure (Printf.sprintf "Got a merge conflict before we expected
+                     one: %s\n%!" s)
+  | `Ok updated_t -> (* all's well; try merging the other branch *)
+    (* hm, not sure what happens if we try to merge into t vs updated_t here;
+       possibly influenced by the fact that t is a store and not a view? *)
+    Irmin.merge "Merging expired_removed into master" exp_branch ~into:t
+    >>= function
+    | `Conflict s -> OUnit.assert_failure (Printf.sprintf "Got a merge conflict
+                                             when we (sort of) expected one:
+                                             %s\n%!" s)
+    | `Ok tree -> OUnit.assert_failure "Didn't get a merge conflict?"
+    
+
 let main () =
   readback_works () >>= fun () ->
-  updates_work ()
+  simple_update_works () >>= fun () -> 
+  merge_conflicts_solved ()
 
 let () =
   Lwt_unix.run (main ())
