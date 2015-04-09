1550a7c96370e71a3b6babd0b36dad40e66789fa
Mindy Preston <meetup@yomimono.org>
2015-04-09 17:46:15 +0100

    actually use timeout_or instead of copypasta

--- lib_test/test_arp.ml
+++ lib_test/test_arp.ml
@@ -38,16 +38,14 @@ let create_returns () =
    make anything nonsensible to feed to `create`, so I'll call that a test the
    typechecker does for us for free *)
 
-let timeout_or (backend, a) do_fn listen_fn =
-  or_error "backend" V.connect backend >>= fun listen_netif ->
+let timeout_or ~timeout ~msg listen_netif do_fn listen_fn =
   (* set up a listener on listen_netif that fails if it doesn't hear a GARP
      within some small number of seconds *)
   Lwt.join [
-    do_fn ;
+    do_fn ();
     (Lwt.pick [
-        V.listen listen_netif listen_fn;
-      Lwt_unix.sleep 0.1 >>= fun () -> OUnit.assert_failure "100ms timeout
-      exceeded before listen_fn returned"
+        V.listen listen_netif (listen_fn ());
+      Lwt_unix.sleep timeout >>= fun () -> OUnit.assert_failure msg
     ])
   ]
 
@@ -58,20 +56,19 @@ let set_ips () =
 (* TODO: according to the contract in arpv4.mli, add_ip, set_ip, and remove_ip
    are supposed to emit GARP packets; we should make sure that actually happens
    generalize this test for use in other functions *)
-  Lwt.join [
-    (A.set_ips a [ my_ip ] >>= fun a ->
+  let do_fn () =
+    A.set_ips a [ my_ip ] >>= fun a ->
     OUnit.assert_equal [ my_ip ] (A.get_ips a);
-    Lwt.return_unit) ;
-    (Lwt.pick [
-      V.listen listen_netif 
-        (fun buf -> match A.is_garp my_ip buf with 
-           | false -> OUnit.assert_failure "something non-GARP sent after set_ips" 
-           | true -> V.disconnect listen_netif
-        );
-      Lwt_unix.sleep 0.1 >>= fun () -> OUnit.assert_failure "GARP not emitted
-      within 100ms of call to set_ips"
-    ])
-  ] >>= fun () ->
+    Lwt.return_unit
+  in
+  let listen_fn () =
+    (fun buf -> match A.is_garp my_ip buf with 
+       | false -> OUnit.assert_failure "something non-GARP sent after set_ips" 
+       | true -> V.disconnect listen_netif
+    )
+  in
+  timeout_or ~timeout:0.1 ~msg:"100ms timeout exceeded before listen_fn returned" 
+    listen_netif do_fn listen_fn >>= fun () -> 
   A.set_ips a [] >>= fun a -> 
   OUnit.assert_equal [] (A.get_ips a);
   A.set_ips a [ my_ip; Ipaddr.V4.of_string_exn "10.20.1.1" ] >>= fun a ->
