070b7c2353057b4ce03f7e030481b4b9b6601b42
Mindy Preston <meetup@yomimono.org>
2015-06-10 14:40:20 +0100

    test unicast reply receipt & cache update

--- lib_test/test_arp.ml
+++ lib_test/test_arp.ml
@@ -3,8 +3,12 @@ open Common
 
 let first_ip = Ipaddr.V4.of_string_exn "192.168.3.1"
 let second_ip = Ipaddr.V4.of_string_exn "192.168.3.10"
-let first_mac = Macaddr.of_string_exn "00:16:3e:00:11:00"
-let second_mac = Macaddr.of_string_exn "10:9a:dd:c0:ff:ee"
+let sample_mac = Macaddr.of_string_exn "10:9a:dd:c0:ff:ee"
+
+let send_buf_sleep_then_dc speak_netif listen_netif bufs () =
+  Lwt.join (List.map (V.write speak_netif) bufs) >>= fun () ->
+  Lwt_unix.sleep 0.1 >>= fun () ->
+  V.disconnect listen_netif
 
 let create_is_consistent () =
   (* Arp.create returns something bearing resemblance to an Arp.t *)
@@ -87,9 +91,9 @@ let get_remove_ips () =
   OUnit.assert_equal [] (A_fs.get_ips a);
   Lwt.return_unit
 
-let input_single_reply () =
+let input_single_garp () =
   (* use on-disk git fs for cache so we can read it back and check it ourselves *)
-  let root = root ^ "/input_single_reply" in
+  let root = root ^ "/input_single_garp" in
   let backend = B.create () in
   get_arp ~backend ~root:(root ^ "/listener") () >>=
   fun (listen_config, backend, listen_netif, listen_ethif, listen_arp) ->
@@ -98,7 +102,7 @@ let input_single_reply () =
   (* send a GARP from one side (speak_arp) and make sure it was heard on the
      other *)
   timeout_or ~timeout:0.1 ~msg:"Nothing received by listen_netif when trying to
-  do single reply test"
+  do single GARP input test"
     listen_netif (fun () -> A_fs.set_ips speak_arp [ first_ip ] )
     (fun () -> fun buf -> A_fs.input listen_arp buf >>= fun () -> V.disconnect listen_netif)
   >>= fun () ->
@@ -114,6 +118,44 @@ let input_single_reply () =
       Lwt.return_unit
     | Pending _ -> OUnit.assert_failure "Pending entry for an entry that had a
   GARP emitted on the same vnetif backend"
+  with
+    Not_found -> OUnit.assert_failure "Expected cache entry not found in
+    listener cache map, as read back from Irmin"
+
+let input_single_unicast () =
+  (* use on-disk git fs for cache so we can read it back and check it ourselves *)
+  let root = root ^ "/input_single_unicast" in
+  let backend = B.create () in
+  get_arp ~backend ~root:(root ^ "/listener") () >>=
+  fun (listen_config, backend, listen_netif, listen_ethif, listen_arp) ->
+  get_arp ~backend ~root:(root ^ "/speaker") () >>=
+  fun (_, backend, speak_netif, speak_ethif, speak_arp) ->
+  (* send an ARP reply from one side (speak_arp) and make sure it was heard on the
+     other *)
+  (* in order to package this properly, we need mac details for each netif *)
+  let for_listener = Irmin_arp.Arp.Parse.cstruct_of_arp
+      { Irmin_arp.Arp.op = `Reply; 
+        sha = (V.mac speak_netif); 
+        tha = (V.mac listen_netif); spa = first_ip;
+        tpa = second_ip } in
+  timeout_or ~timeout:0.1 ~msg:"Nothing received by listen_netif when trying to
+  do single unicast reply input test"
+    listen_netif (fun () -> V.write speak_netif for_listener)
+    (fun () -> fun buf -> A_fs.input listen_arp buf >>= fun () -> V.disconnect listen_netif)
+  >>= fun () ->
+  (* listen_config should have the ARP cache history reflecting the updates send
+     by speak_arp; a current read should show us first_ip *)
+  let store = Irmin.basic (module Irmin_backend_fs) (module T) in
+  Irmin.create store listen_config Irmin_unix.task >>= fun store ->
+  Irmin.read_exn (store "readback of map") T.Path.empty >>= fun map ->
+  (* TODO: iterate over the commit history of IPs *)
+  try
+    let open Entry in
+    match T.find first_ip map with
+    | Confirmed (time, entry) -> OUnit.assert_equal entry (V.mac speak_netif);
+      Lwt.return_unit
+    | Pending _ -> OUnit.assert_failure "Pending entry for an entry that had a
+  unicast reply emitted on the same vnetif backend"
   with
     Not_found -> OUnit.assert_failure "Expected cache entry not found in
     listener cache map, as read back from Irmin"
@@ -167,13 +209,9 @@ let input_garbage () =
       ~ipv4:(fun buf -> Lwt.return_unit) ~ipv6:(fun buf -> Lwt.return_unit)
       listen_ethif)
   in
+  let fire_away = send_buf_sleep_then_dc speak_netif listen_netif in
   (* TODO: this is a good candidate for a property test -- `parse` on an arbitrary
      buffer of size less than k always returns `Too_short *)
-  let send_buf_sleep_then_dc bufs () =
-    Lwt.join (List.map (V.write speak_netif) bufs) >>= fun () ->
-    Lwt_unix.sleep 0.1 >>= fun () ->
-    V.disconnect listen_netif
-  in
   let listener_mac = V.mac listen_netif in
   let speaker_mac = V.mac speak_netif in
   (* don't keep entries for unicast replies to someone else *)
@@ -192,7 +230,7 @@ let input_garbage () =
   let claiming_ours = Irmin_arp.Arp.Parse.cstruct_of_arp
       { Irmin_arp.Arp.op = `Reply; sha = speaker_mac; tha = listener_mac; spa = first_ip;
         tpa = first_ip } in
-  Lwt.join [ listen_fn (); send_buf_sleep_then_dc
+  Lwt.join [ listen_fn (); fire_away
                [(Cstruct.create 0); for_someone_else;
                 claiming_broadcast; claiming_ours ] () ]
   >>= fun () ->
@@ -236,6 +274,8 @@ let parse_zeros () =
 
 let parse_unparse () =
   let module P = Irmin_arp.Arp.Parse in
+  let first_mac = Macaddr.of_string_exn "00:16:3e:00:11:00" in
+  let second_mac = Macaddr.of_string_exn "10:9a:dd:c0:ff:ee" in
   let test_arp = { Irmin_arp.Arp.op = `Request;
                    sha = first_mac; spa = first_ip;
                    tha = second_mac; tpa = second_ip; } in
@@ -259,7 +299,7 @@ let query_with_seeded_cache () =
     | None -> OUnit.assert_failure "Couldn't read store from query_for_seeded_map"
     | Some map ->
       OUnit.assert_equal T.empty map;
-      let seeded = T.add second_ip (Entry.Confirmed ((Clock.time () +. 60.), second_mac)) map in
+      let seeded = T.add second_ip (Entry.Confirmed ((Clock.time () +. 60.), sample_mac)) map in
       Irmin.update (store "query_with_seeded_cache: seed cache entry") T.Path.empty
         seeded >>= fun () ->
       (* OK, we've written an entry, so now calling query for that key
@@ -267,7 +307,7 @@ let query_with_seeded_cache () =
       timeout_or ~timeout:0.5 ~msg:"Query sent for something that was seeded in
         the cache" listen_netif
         (fun () -> A_fs.query speak_arp second_ip >>= function
-        | `Ok mac when mac = second_mac -> (* yay! *)
+        | `Ok mac when mac = sample_mac -> (* yay! *)
           V.disconnect listen_netif
         | `Ok mac -> OUnit.assert_failure (Printf.sprintf "pre-seeded query got a
     MAC, but it's the wrong one: %s" (Macaddr.to_string mac))
@@ -323,7 +363,7 @@ let entries_aged_out () =
     "entries_aged_out" >>= fun our_branch ->
   Irmin.read_exn (our_branch "readback of map") T.Path.empty >>= fun init_map ->
   let seeded = T.add second_ip (Entry.Confirmed ((Clock.time () -. 9999999.),
-                                                 second_mac)) init_map in
+                                                 sample_mac)) init_map in
   Irmin.update (our_branch "entries_aged_out: seed cache entry") T.Path.empty
     seeded >>= fun () ->
   Irmin.merge_exn "entries_aged_out: merge seeded ARP entry" our_branch
@@ -356,7 +396,8 @@ let () =
     "query_sent_with_empty_cache", `Slow, lwt_run query_sent_with_empty_cache;
   ] in
   let input = [
-    "input_single_reply", `Slow, lwt_run input_single_reply;
+    "input_single_garp", `Slow, lwt_run input_single_garp;
+    "input_single_unicast_reply", `Slow, lwt_run input_single_unicast;
     "input_changed_ip", `Slow, lwt_run input_changed_ip ;
     "input_garbage", `Slow, lwt_run input_garbage
   ] in
