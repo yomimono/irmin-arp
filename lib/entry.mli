a0640713f13b6abc859d8780c08b1383ae2e902f
Mindy Preston <meetup@yomimono.org>
2015-06-11 13:57:57 +0100

    remove Pending variant from entry type
    Conflicts:
    	lib/table.ml

--- lib/entry.mli
+++ lib/entry.mli
@@ -1,6 +1,5 @@
 type result = [ `Ok of Macaddr.t | `Timeout ]
 type t = 
-  | Pending of result Lwt.t * result Lwt.u
   | Confirmed of float * Macaddr.t
 
 val to_string : t -> string
@@ -8,5 +7,3 @@ val to_json : t -> Ezjsonm.value
 val of_json : Ezjsonm.value -> t option
 val compare : t -> t -> int
 val make_confirmed : float -> Macaddr.t -> t
-val make_pending : (result Lwt.t * result Lwt.u) -> t
-val is_pending : t -> bool
