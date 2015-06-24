2e12e1cc455f7b7fb9b5a445a854b82ec5b10932
Mindy Preston <meetup@yomimono.org>
2015-06-24 14:45:28 +0100

    table.ml: remove printf; improve readability

--- lib/table.ml
+++ lib/table.ml
@@ -59,12 +59,11 @@ module Make(P: Irmin.Path.S) = struct
   let find = M.find
   let empty = M.empty
   let expire t now =
-    M.filter (fun _ip entry -> match entry with
+    let check_expiry _ip = function
         | Entry.Pending _ -> true
-        | Entry.Confirmed (time, _) ->
-          Printf.printf "time %f > current time %f: %s\n%!" time now
-            (string_of_bool (time > now));
-          time > now) t
+        | Entry.Confirmed (time, _) -> time > now
+    in
+    M.filter check_expiry t
 
   let merge _path ~(old : Entry.t M.t Irmin.Merge.promise) t1 t2 = 
     let open Irmin.Merge.OP in
